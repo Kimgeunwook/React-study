f730b62a6f152984832cacdbe9082c5dce8a9378
wook9 <wook961206@naver.com>
2020-06-12 16:41:09 +0900

    first commit

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 import Hello from './Hello.js';
 function App() {
   return (
-    <div>
-     <Hello />
-     <Hello />
-     <Hello />
-    </div>
-  );
+    <Hello name = "김근욱"/>
+  )
 }
 
 export default App;
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,6 +1,6 @@
 import React from 'react';
-function Hello()
+function Hello({aa})
 {
-    return <div> 안녕하세요!</div>;
+    return <div> 안녕하세요!{aa}님p2</div>;
 }
 export default Hello;
